7d7b4e842b6ab243b941b2d6a267746500c74c4f
Daniel Webb <daniel.webb@havenpower.com>
2021-01-15 08:51:22 +0000

    Adding edit recipient component logic and template

--- src/app/recipients/recipients-edit/recipients-edit.component.ts
+++ src/app/recipients/recipients-edit/recipients-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/models/appState';
+import { RecipientService } from '../recipient.service';
 
 @Component({
   selector: 'app-recipients-edit',
@@ -7,9 +12,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipientsEditComponent implements OnInit {
 
-  constructor() { }
+  recipientForm: FormGroup;
+  isNewRecipient: boolean;
+  recipientId: any;
 
-  ngOnInit(): void {
+  departments = [ // TODO update to http get call
+    { id: 1, value: 'CM Acc Rec/Insolvency' },
+    { id: 2, value: 'CM Collections' },
+    { id: 3, value: 'Complaint Escalations' },
+    { id: 4, value: 'Complaints' },
+    { id: 5, value: 'Contact' },
+    { id: 6, value: 'COT/Trace' },
+    { id: 7, value: 'Direct Admin' },
+    { id: 8, value: 'Direct Sales' },
+    { id: 9, value: 'Enterprise Billing' },
+    { id: 10, value: 'Escalated Debts' },
+    { id: 11, value: 'Ledger' },
+    { id: 12, value: 'N/A' },
+    { id: 13, value: 'On/Off Boarding' },
+    { id: 14, value: 'Operations Team Leaders' },
+    { id: 15, value: 'Renewals' },
+    { id: 16, value: 'Smart Metering' },
+    { id: 17, value: 'Support' },
+    { id: 18, value: 'Workflow' }
+  ]
+
+  constructor(private store: Store<AppState>,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private recipientsService: RecipientService,
+    private fb: FormBuilder) {
   }
 
+  ngOnInit(): void {
+    this.recipientId = this.activatedRoute.snapshot.params['id'];
+    this.isNewRecipient = this.recipientId === 'new';
+    this.buildForm();
+    if (!this.isNewRecipient) {
+      this.getRecipient();
+    }
+  }
+
+  getRecipient() {
+    this.recipientsService.loadRecipient(this.recipientId)
+    .subscribe(recipient => this.recipientForm.patchValue(recipient));
+  }
+
+  buildForm() {
+    this.recipientForm = this.fb.group({
+      id: [null],
+      EmailAddress: ['', [Validators.required, Validators.email]],
+      Department: ['', Validators.required]
+    })
+  }
+
+  goBack() {
+    this.router.navigate(['recipients']);
+  }
+
+  saveRecipient() {
+    if (this.isNewRecipient) {
+      this.recipientsService.addRecipient(this.recipientForm.value)
+      .subscribe(() => this.router.navigate(['recipients']));
+    } else {
+      const recipientToUpdate = { ...this.recipientForm.value };
+      this.recipientsService.updateRecipient(recipientToUpdate, this.recipientId)
+      .subscribe(() => this.router.navigate(['recipients']));
+    }
+  }
 }
